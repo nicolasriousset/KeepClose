f8ca70f621f477ecc6297c433eb5685d94ff9122
Nicolas <nicolas@riousset.com>
2025-08-31 12:48:05 +0200

    Mise en forme du code

--- KeepClose.ino
+++ KeepClose.ino
@@ -13,78 +13,78 @@ int lastRSSI = 0;
 lv_obj_t* label_btn1 = NULL;
 
 
-#define ENDIAN_CHANGE_U16(x) ((((x) & 0xFF00) >> 8) + (((x) & 0xFF) << 8))
+#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00) >> 8) + (((x)&0xFF) << 8))
 
-int         scanTime = 5 * 1000; // In milliseconds
+int scanTime = 5 * 1000;  // In milliseconds
 
 class ScanCallbacks : public NimBLEScanCallbacks {
-    void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override {
-        if (advertisedDevice->haveName()) {
-            Serial.print("Device name: ");
-            Serial.println(advertisedDevice->getName().c_str());
-            Serial.println("");
-        }
-
-        if (advertisedDevice->haveServiceUUID()) {
-            NimBLEUUID devUUID = advertisedDevice->getServiceUUID();
-            Serial.print("Found ServiceUUID: ");
-            Serial.println(devUUID.toString().c_str());
-            Serial.println("");
-        } else if (advertisedDevice->haveManufacturerData() == true) {
-            std::string strManufacturerData = advertisedDevice->getManufacturerData();
-            if (strManufacturerData.length() == 25 && strManufacturerData[0] == 0x4C && strManufacturerData[1] == 0x00) {
-                Serial.println("Found an iBeacon!");
-                NimBLEBeacon oBeacon = NimBLEBeacon();
-                oBeacon.setData(reinterpret_cast<const uint8_t*>(strManufacturerData.data()), strManufacturerData.length());
-                Serial.printf("iBeacon Frame\n");
-                Serial.printf("ID: %04X Major: %d Minor: %d UUID: %s Power: %d\n",
-                              oBeacon.getManufacturerId(),
-                              ENDIAN_CHANGE_U16(oBeacon.getMajor()),
-                              ENDIAN_CHANGE_U16(oBeacon.getMinor()),
-                              oBeacon.getProximityUUID().toString().c_str(),
-                              oBeacon.getSignalPower());
-            } else {
-                Serial.println("Found another manufacturers beacon!");
-                Serial.printf("strManufacturerData: %d ", strManufacturerData.length());
-                for (int i = 0; i < strManufacturerData.length(); i++) {
-                    Serial.printf("[%X]", strManufacturerData[i]);
-                }
-                Serial.printf("\n");
-            }
-            return;
-        }
-
-        NimBLEUUID eddyUUID = (uint16_t)0xfeaa;
-
-        if (advertisedDevice->getServiceUUID().equals(eddyUUID)) {
-            std::string serviceData = advertisedDevice->getServiceData(eddyUUID);
-            if (serviceData[0] == 0x20) {
-                Serial.println("Found an EddystoneTLM beacon!");
-                NimBLEEddystoneTLM foundEddyTLM = NimBLEEddystoneTLM();
-                foundEddyTLM.setData(reinterpret_cast<const uint8_t*>(serviceData.data()), serviceData.length());
-
-                Serial.printf("Reported battery voltage: %dmV\n", foundEddyTLM.getVolt());
-                Serial.printf("Reported temperature from TLM class: %.2fC\n", (double)foundEddyTLM.getTemp());
-                int   temp     = (int)serviceData[5] + (int)(serviceData[4] << 8);
-                float calcTemp = temp / 256.0f;
-                Serial.printf("Reported temperature from data: %.2fC\n", calcTemp);
-                Serial.printf("Reported advertise count: %d\n", foundEddyTLM.getCount());
-                Serial.printf("Reported time since last reboot: %ds\n", foundEddyTLM.getTime());
-                Serial.println("\n");
-                Serial.print(foundEddyTLM.toString().c_str());
-                Serial.println("\n");
-            }
-        }
+  void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override {
+    if (advertisedDevice->haveName()) {
+      Serial.print("Device name: ");
+      Serial.println(advertisedDevice->getName().c_str());
+      Serial.println("");
     }
+
+    if (advertisedDevice->haveServiceUUID()) {
+      NimBLEUUID devUUID = advertisedDevice->getServiceUUID();
+      Serial.print("Found ServiceUUID: ");
+      Serial.println(devUUID.toString().c_str());
+      Serial.println("");
+    } else if (advertisedDevice->haveManufacturerData() == true) {
+      std::string strManufacturerData = advertisedDevice->getManufacturerData();
+      if (strManufacturerData.length() == 25 && strManufacturerData[0] == 0x4C && strManufacturerData[1] == 0x00) {
+        Serial.println("Found an iBeacon!");
+        NimBLEBeacon oBeacon = NimBLEBeacon();
+        oBeacon.setData(reinterpret_cast<const uint8_t*>(strManufacturerData.data()), strManufacturerData.length());
+        Serial.printf("iBeacon Frame\n");
+        Serial.printf("ID: %04X Major: %d Minor: %d UUID: %s Power: %d\n",
+                      oBeacon.getManufacturerId(),
+                      ENDIAN_CHANGE_U16(oBeacon.getMajor()),
+                      ENDIAN_CHANGE_U16(oBeacon.getMinor()),
+                      oBeacon.getProximityUUID().toString().c_str(),
+                      oBeacon.getSignalPower());
+      } else {
+        Serial.println("Found another manufacturers beacon!");
+        Serial.printf("strManufacturerData: %d ", strManufacturerData.length());
+        for (int i = 0; i < strManufacturerData.length(); i++) {
+          Serial.printf("[%X]", strManufacturerData[i]);
+        }
+        Serial.printf("\n");
+      }
+      return;
+    }
+
+    NimBLEUUID eddyUUID = (uint16_t)0xfeaa;
+
+    if (advertisedDevice->getServiceUUID().equals(eddyUUID)) {
+      std::string serviceData = advertisedDevice->getServiceData(eddyUUID);
+      if (serviceData[0] == 0x20) {
+        Serial.println("Found an EddystoneTLM beacon!");
+        NimBLEEddystoneTLM foundEddyTLM = NimBLEEddystoneTLM();
+        foundEddyTLM.setData(reinterpret_cast<const uint8_t*>(serviceData.data()), serviceData.length());
+
+        Serial.printf("Reported battery voltage: %dmV\n", foundEddyTLM.getVolt());
+        Serial.printf("Reported temperature from TLM class: %.2fC\n", (double)foundEddyTLM.getTemp());
+        int temp = (int)serviceData[5] + (int)(serviceData[4] << 8);
+        float calcTemp = temp / 256.0f;
+        Serial.printf("Reported temperature from data: %.2fC\n", calcTemp);
+        Serial.printf("Reported advertise count: %d\n", foundEddyTLM.getCount());
+        Serial.printf("Reported time since last reboot: %ds\n", foundEddyTLM.getTime());
+        Serial.println("\n");
+        Serial.print(foundEddyTLM.toString().c_str());
+        Serial.println("\n");
+      }
+    }
+  }
 } scanCallbacks;
 
 void initNimBLE() {
-    NimBLEDevice::init("Beacon-scanner");
-    pBLEScan = BLEDevice::getScan();
-    pBLEScan->setScanCallbacks(&scanCallbacks);
-    pBLEScan->setActiveScan(true);
-    pBLEScan->setInterval(100);
-    pBLEScan->setWindow(100);
+  NimBLEDevice::init("Beacon-scanner");
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setScanCallbacks(&scanCallbacks);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(100);
 }
 
 void setup() {
@@ -117,19 +117,18 @@ void setup() {
   lv_label_set_text(label2, "Toggled");
 }
 
-static void event_handler(lv_obj_t *obj, lv_event_t event)
-{
-    if (event == LV_EVENT_CLICKED) {
-        Serial.printf("Clicked\n");
-        ttgo->motor->onec();
+static void event_handler(lv_obj_t* obj, lv_event_t event) {
+  if (event == LV_EVENT_CLICKED) {
+    Serial.printf("Clicked\n");
+    ttgo->motor->onec();
     NimBLEScanResults foundDevices = pBLEScan->getResults(scanTime, false);
     Serial.print("Devices found: ");
     Serial.println(foundDevices.getCount());
     Serial.println("Scan done!");
-    pBLEScan->clearResults(); // del
-    } else if (event == LV_EVENT_VALUE_CHANGED) {
-        Serial.printf("Toggled\n");
-    }
+    pBLEScan->clearResults();  // del
+  } else if (event == LV_EVENT_VALUE_CHANGED) {
+    Serial.printf("Toggled\n");
+  }
 }
 
 void loop() {
