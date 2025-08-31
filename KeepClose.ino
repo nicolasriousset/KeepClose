beb0c9db537bcaccb0c6b39e9a613eb2799921c5
Nicolas <nicolas@riousset.com>
2025-08-31 12:47:38 +0200

    La montre détecte enfin les balises à proximité

--- KeepClose.ino
+++ KeepClose.ino
@@ -1,138 +1,90 @@
 #include "config.h"
-#include "NimBLEDevice.h"
+#include <NimBLEDevice.h>
+#include <NimBLEAdvertisedDevice.h>
+#include "NimBLEEddystoneTLM.h"
+#include "NimBLEBeacon.h"
 
 TTGOClass* ttgo;
 NimBLEScan* pBLEScan;
 bool beaconFound = false;
-bool scanInProgress = false;
 
 
 int lastRSSI = 0;
 lv_obj_t* label_btn1 = NULL;
 
 
-// Classe callback pour traiter les résultats du scan
-class MyScanCallbacks : public NimBLEScanCallbacks {
-public:
-  void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override {
-  }
+#define ENDIAN_CHANGE_U16(x) ((((x) & 0xFF00) >> 8) + (((x) & 0xFF) << 8))
 
-  void onScanEnd(const NimBLEScanResults& scanResults, int reason) override {
-    Serial.println("=== Scan terminé via callback ===");
-    scanInProgress = false;
+int         scanTime = 5 * 1000; // In milliseconds
 
-    int deviceCount = scanResults.getCount();
-    Serial.printf("Nombre total d'appareils: %d\n", deviceCount);
-
-    // Traiter tous les résultats ici
-    for (int i = 0; i < deviceCount; i++) {
-      const NimBLEAdvertisedDevice* device = scanResults.getDevice(i);
-
-      if (device->haveName()) {
-        std::string deviceName = device->getName();
-        Serial.printf("Final - Appareil %d: %s (RSSI: %d)\n", i + 1, deviceName.c_str(), device->getRSSI());
-
-        if (deviceName == "Holy-IOT") {
-          Serial.println("*** Holy-IOT trouvé dans onScanEnd! ***");
-          lastRSSI = device->getRSSI();
-
-          // Mettre à jour l'interface utilisateur
-          if (label_btn1) {
-            lv_label_set_text_fmt(label_btn1, "RSSI: %d", lastRSSI);
-          }
+class ScanCallbacks : public NimBLEScanCallbacks {
+    void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override {
+        if (advertisedDevice->haveName()) {
+            Serial.print("Device name: ");
+            Serial.println(advertisedDevice->getName().c_str());
+            Serial.println("");
         }
-      }
+
+        if (advertisedDevice->haveServiceUUID()) {
+            NimBLEUUID devUUID = advertisedDevice->getServiceUUID();
+            Serial.print("Found ServiceUUID: ");
+            Serial.println(devUUID.toString().c_str());
+            Serial.println("");
+        } else if (advertisedDevice->haveManufacturerData() == true) {
+            std::string strManufacturerData = advertisedDevice->getManufacturerData();
+            if (strManufacturerData.length() == 25 && strManufacturerData[0] == 0x4C && strManufacturerData[1] == 0x00) {
+                Serial.println("Found an iBeacon!");
+                NimBLEBeacon oBeacon = NimBLEBeacon();
+                oBeacon.setData(reinterpret_cast<const uint8_t*>(strManufacturerData.data()), strManufacturerData.length());
+                Serial.printf("iBeacon Frame\n");
+                Serial.printf("ID: %04X Major: %d Minor: %d UUID: %s Power: %d\n",
+                              oBeacon.getManufacturerId(),
+                              ENDIAN_CHANGE_U16(oBeacon.getMajor()),
+                              ENDIAN_CHANGE_U16(oBeacon.getMinor()),
+                              oBeacon.getProximityUUID().toString().c_str(),
+                              oBeacon.getSignalPower());
+            } else {
+                Serial.println("Found another manufacturers beacon!");
+                Serial.printf("strManufacturerData: %d ", strManufacturerData.length());
+                for (int i = 0; i < strManufacturerData.length(); i++) {
+                    Serial.printf("[%X]", strManufacturerData[i]);
+                }
+                Serial.printf("\n");
+            }
+            return;
+        }
+
+        NimBLEUUID eddyUUID = (uint16_t)0xfeaa;
+
+        if (advertisedDevice->getServiceUUID().equals(eddyUUID)) {
+            std::string serviceData = advertisedDevice->getServiceData(eddyUUID);
+            if (serviceData[0] == 0x20) {
+                Serial.println("Found an EddystoneTLM beacon!");
+                NimBLEEddystoneTLM foundEddyTLM = NimBLEEddystoneTLM();
+                foundEddyTLM.setData(reinterpret_cast<const uint8_t*>(serviceData.data()), serviceData.length());
+
+                Serial.printf("Reported battery voltage: %dmV\n", foundEddyTLM.getVolt());
+                Serial.printf("Reported temperature from TLM class: %.2fC\n", (double)foundEddyTLM.getTemp());
+                int   temp     = (int)serviceData[5] + (int)(serviceData[4] << 8);
+                float calcTemp = temp / 256.0f;
+                Serial.printf("Reported temperature from data: %.2fC\n", calcTemp);
+                Serial.printf("Reported advertise count: %d\n", foundEddyTLM.getCount());
+                Serial.printf("Reported time since last reboot: %ds\n", foundEddyTLM.getTime());
+                Serial.println("\n");
+                Serial.print(foundEddyTLM.toString().c_str());
+                Serial.println("\n");
+            }
+        }
     }
-
-    if (!beaconFound) {
-      Serial.println("⚠ Aucune balise Holy-IOT trouvée.");
-    }
-  }
-
-  // Fonction pour décoder les URLs Eddystone
-  String decodeEddystoneURL(const std::string& data) {
-    if (data.length() < 3) return "";
-
-    // Table des préfixes URL Eddystone
-    const char* urlPrefixes[] = {
-      "http://www.",
-      "https://www.",
-      "http://",
-      "https://"
-    };
-
-    // Table des suffixes URL Eddystone
-    const char* urlSuffixes[] = {
-      ".com/", ".org/", ".edu/", ".net/", ".info/", ".biz/", ".gov/",
-      ".com", ".org", ".edu", ".net", ".info", ".biz", ".gov"
-    };
-
-    String url = "";
-    uint8_t prefixCode = data[2];
-
-    if (prefixCode < 4) {
-      url += urlPrefixes[prefixCode];
-    }
-
-    // Décoder le reste de l'URL
-    for (size_t i = 3; i < data.length(); i++) {
-      uint8_t c = data[i];
-      if (c < 14) {
-        url += urlSuffixes[c];
-      } else if (c >= 32 && c <= 126) {
-        url += (char)c;
-      }
-    }
-
-    return url;
-  }
-};
-
-
+} scanCallbacks;
 
 void initNimBLE() {
-  // Initialiser NimBLE
-  Serial.println("Initialisation NimBLE...");
-  NimBLEDevice::init("");
-
-  // Créer le scanner BLE
-  pBLEScan = NimBLEDevice::getScan();
-
-  // Configurer le callback avec la classe MyScanCallbacks
-  pBLEScan->setScanCallbacks(new MyScanCallbacks(), true);  // true pour recevoir les doublons
-
-  // Configuration du scan
-  pBLEScan->setActiveScan(true);        // Scan actif pour plus d'informations
-  pBLEScan->setInterval(100);           // Intervalle en ms
-  pBLEScan->setWindow(99);              // Fenêtre de scan en ms
-  pBLEScan->setDuplicateFilter(false);  // Permettre les doublons pour suivi continu
-
-  Serial.println("Scanner BLE configuré.");
-  Serial.println("Recherche de la balise Holy-IOT...");
-  Serial.println();
-}
-
-void startScanAsync() {
-  if (scanInProgress) {
-    Serial.println("Scan déjà en cours...");
-    return;
-  }
-  
-  Serial.println(">>> Démarrage du scan asynchrone...");
-  
-  // Nettoyer les résultats précédents
-  pBLEScan->clearResults();
-  scanInProgress = true;
-  
-  // Lancer un scan de 5 secondes
-  bool scanStarted = pBLEScan->start(5, false);
-  
-  if (scanStarted) {
-    Serial.println("Scan asynchrone démarré. Résultats via callback.");
-  } else {
-    Serial.println("Erreur lors du démarrage du scan.");
-    scanInProgress = false;
-  }
+    NimBLEDevice::init("Beacon-scanner");
+    pBLEScan = BLEDevice::getScan();
+    pBLEScan->setScanCallbacks(&scanCallbacks);
+    pBLEScan->setActiveScan(true);
+    pBLEScan->setInterval(100);
+    pBLEScan->setWindow(100);
 }
 
 void setup() {
@@ -165,18 +117,23 @@ void setup() {
   lv_label_set_text(label2, "Toggled");
 }
 
-static void event_handler(lv_obj_t* obj, lv_event_t event) {
-  if (event == LV_EVENT_CLICKED) {
-    Serial.printf("Clicked\n");
-    ttgo->motor->onec();
-    startScanAsync();
-    // Scan BLE Holy-IOT
-  } else if (event == LV_EVENT_VALUE_CHANGED) {
-    Serial.printf("Toggled\n");
-  }
+static void event_handler(lv_obj_t *obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED) {
+        Serial.printf("Clicked\n");
+        ttgo->motor->onec();
+    NimBLEScanResults foundDevices = pBLEScan->getResults(scanTime, false);
+    Serial.print("Devices found: ");
+    Serial.println(foundDevices.getCount());
+    Serial.println("Scan done!");
+    pBLEScan->clearResults(); // del
+    } else if (event == LV_EVENT_VALUE_CHANGED) {
+        Serial.printf("Toggled\n");
+    }
 }
 
 void loop() {
   lv_task_handler();
+
   delay(5);
 }
